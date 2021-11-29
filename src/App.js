0341c104e9ee46111dd9e4664974a430dee75f4b
AhmedElkharadly <ahmed.e.elkharadly@gmail.com>
2021-11-29 23:51:24 +0200

    Adding twitter link

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { Routes , Route, Link } from "react-router-dom";
 import './App.css';
+import Login from './component/login/login';
+import LoginR from "./component/login/loginR";
+import LeftPane from "./component/leftpane/leftpane";
+import RightPane from "./component/rightpane/rightpane";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+        <LeftPane/>
+      <div>
+        
+        <Routes >
+          <Route exact path="/home">Home Page</Route>
+          <Route exact path="/explore">explore Page</Route>
+          <Route exact path="/notifications">notification Page</Route>
+          <Route exact path="/messages">message Page</Route>
+          <Route exact path="/bookmarks">bookmarks Page</Route>
+          <Route exact path="/lists">list Page</Route>
+          <Route exact path="/profile">Profile Page</Route>
+        </Routes > 
+      </div>
+        <RightPane/>
     </div>
   );
 }
